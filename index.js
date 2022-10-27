49d9a22edb692771b9229fa48bc433074909c993
taro <lautaro@airtm.io>
2022-10-27 00:48:08 -0300

    ignore non .bmp files

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import { encryptBitmap } from './encryptBitmap.js'
 
 const outputDir = './output'
 const files = await fs.readdir('./images')
+const bitmapFiles = files.filter(name => name.endsWith('.bmp'))
 
 await fs.mkdir(outputDir, { recursive: true });
 
-for (const file of files) {
+console.log('Will encrypt', bitmapFiles.length, 'files')
+
+for (const file of bitmapFiles) {
   console.log('Encrypting', file)
   await encryptBitmap(`./images/${file}`, `${outputDir}/${file}`)
 }
